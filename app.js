c0979436543defd46241ae374b6640b76098f2ed
Roberto Zalazar <servicios773@gmail.com>
2023-07-27 22:20:58 -0300

    agregando rutas

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3030;
-
+const PORT = 3000;
 
 
 app.use(express.static('public'));
 
+
 const viewsPath = path.join(__dirname, 'views');
 
-app.get('/',(req, res) =>{
-    res.sendFile(path.join(viewsPath, 'home.html'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(viewsPath, 'home.html'));
 });
 
 
-app.listen(PORT, () =>{
-    console.log(`Servidor corriendo en http://localhost:${PORT}`)
-})
+const routes = ['createAccount', 'login' ];
+
+routes.forEach(route => {
+  app.get(`/${route}`, (req, res) => {
+    res.sendFile(path.join(viewsPath, `${route}.html`));
+  });
+});
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(viewsPath, '404.html'));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
